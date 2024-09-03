730948fb628ccef5e50ff2afcb2e7db5d32c7e3b
OREO5522 <krissda5552@gmail.com>
2024-09-04 01:03:47 +0700

    Update a_func.php

--- a_func.php
+++ a_func.php
@@ -3,7 +3,7 @@ session_start();
 $host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db =  "oreo";
+$db =  "OREO";
 //connect to database
 $conn = new PDO("mysql:host=$host;dbname=$db", $db_user, $db_pass);
 $conn->exec("set names utf8mb4");
